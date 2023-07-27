dafcbce9350549a45602f8c467f83000888c30e5
alam2025 <alam13100852@gmail.com>
2023-07-27 12:09:30 +0600

    Task 1

---
+++ Reverse string.js
@@ -0,0 +1,13 @@
+// Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+const ReverseStr=(str)=>{
+      let newStr=''
+      for(i=str.length-1;i>=0;i--){
+            newStr += str[i]
+      }
+
+      return newStr;
+      
+}
+
+console.log(ReverseStr('Welcome'));
